dfbf8325778caa6d108176e3c3298b1e27973501
Diego Correa da Silva <diegocorrea.cc@gmail.com>
2017-01-31 16:26:13 -0300

    Add files via upload
    add plot graphics

--- analisador.R
+++ analisador.R
@@ -1,12 +1,12 @@
-#Abrindo o arquivo, setando os nomes das colunas
-data <- read.csv(file="embrapaFine.csv", header=TRUE, sep=";",col.names= c("mes","temperatura","P","ETP","ARM","ETR","DEF","EXC","cidade"))
+# Abrindo o arquivo, setando os nomes das colunas
+data <- read.csv(file="embrapaFine.csv", header=TRUE, sep=";",col.names= c("mes","temperatura","P","ETP","ARM","ETR","DEF","EXC","cidade"), encoding= "UFT8")
 
 cat("\n----------------------------------------------\n")
 print("Dados Calculados a partir de todas as entradas")
 print("=Temperatura\n")
-print("--Menor") 
+print("--Menor")
 print(min(as.numeric(sub(",",".",data$temperatura))))
-print("--Media") 
+print("--Media")
 print(mean(as.numeric(sub(",",".",data$temperatura))))
 print("--Maior")
 print(max(as.numeric(sub(",",".",data$temperatura))))
@@ -21,9 +21,9 @@ for (i in levsCidade) {
   cid <- subset(data, cidade == i)
 
   cat("\n+Temperatura: \n")
-  print("---Menor") 
+  print("---Menor")
   print(min(as.numeric(sub(",",".",cid$temperatura))))
-  print("---Media") 
+  print("---Media")
   print(mean(as.numeric(sub(",",".",cid$temperatura))))
   print("---Maior")
   print(max(as.numeric(sub(",",".",cid$temperatura))))
@@ -35,6 +35,22 @@ for (i in levsCidade) {
   print(mean(cid$P))
   print("---Maior")
   print(max(cid$P))
+
+  png(sprintf("images/cidade/temperatura/%s.png",i))
+  plot(as.numeric(sub(",",".",cid$temperatura)), 
+  type="o",
+  col="blue", 
+  main=sprintf("%s\n Mês vs Temperatura\n", i),
+  lwd=3,
+  xlab="Mês",
+  ylab="Temperatura")
+  #grid cria quadrantes no gráfico, essas linhas da divisoria, terão a cor vermelha
+  grid(col="red")
+
+  texto <- sprintf("Menor Temperatura: %f\nMedia: %f\nMaior: %f",min(as.numeric(sub(",",".",cid$temperatura))),mean(as.numeric(sub(",",".",cid$temperatura))),max(as.numeric(sub(",",".",cid$temperatura))))
+  mtext(texto, side=1, valign="center", cex=0.8, halign= "left", mar=c(0,0,0,0), col="black", line=1)  
+
+  dev.off()
 }
 
 ##########################################################
