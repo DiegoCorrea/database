bc97a45c1255203b7db84fc0ec9cf3e8b4d6bf0e
Diego Correa da Silva <diegocorrea.cc@gmail.com>
2017-02-06 18:14:17 -0300

    Add files via upload

--- dispersao.R
+++ dispersao.R
@@ -1,60 +1,49 @@
+#install.packages("calibrate")
+library(calibrate)
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
 # Abrindo o arquivo, setando os nomes das colunas
 database <- read.csv(file="embrapaFine.csv", header=TRUE, sep=";", col.names= c("mes","temperatura","pluviometrico","evaPotencial","armazenHidrico","evaReal","defHidrica","excedHidrico","cidade"), encoding= "UFT8")
-
-cat("\n----------------------------------------------\n")
-print("Dados Calculados a partir de todas as entradas")
-print("=Temperatura\n")
-print("--Menor")
-print(min(as.numeric(sub(",",".",database$temperatura))))
-print("--Media")
-print(mean(as.numeric(sub(",",".",database$temperatura))))
-print("--Maior")
-print(max(as.numeric(sub(",",".",database$temperatura))))
+database$temperatura <- as.numeric(sub(",",".",database$temperatura))
+database$pluviometrico <- as.numeric(sub(",",".",database$pluviometrico))
 
 # http://arademaker.github.io/CA-2012-1/2012-03-06/revisao-R.html
-vetor <- vector()
-vetor2 <- vector()
+vecMediaTemp <- vector()
+vecMediaPluv <- vector()
 
 #####################################################################
-#Carregando todos os nomes de cidades diferentes e colocando em 1 vetor
-levsCidade <- unique( unlist( lapply( database$cidade , levels ) ) )
+#Carregando todos os nomes de cidades diferentes e colocando em 1 vecMediaTemp
+levsMes <- unique(database$mes)
 cat("\n----------------------------------------------\n")
-cat("\nMenor, Maior e Média por Cidade\n")
-for (i in levsCidade) {
-  cat("\nCidade: ", i,"\n")
-  cid <- subset(database, cidade == i)
+cat("\nMedia por Mes\n")
+for (i in levsMes) {
+  mes <- subset(database, mes == i)
 
-  cat("\n+Temperatura: \n")
-  print("---Menor")
-  print(min(as.numeric(sub(",",".",cid$temperatura))))
-  print("---Media")
-  mediaTemp <- mean(as.numeric(sub(",",".",cid$temperatura)))
-  print(mediaTemp)
-  print("---Maior")
-  maxTemp <- max(as.numeric(sub(",",".",cid$temperatura)))
-  print(maxTemp)
+  mediaTemp <- mean(mes$temperatura)
 
-  mediaPluv <- mean(as.numeric(cid$pluviometrico))
+  mediaPluv <- mean(mes$pluviometrico)
   print("Media mediaPluv")
   print(mediaPluv)
 
-  vetor[i] <- mediaTemp
-  cat("======>> mediaTemp do vetor = ", vetor[i])
-  vetor2[i] <- mediaPluv
-  cat("======>> mediaPluv do vetor2 = ", vetor2[i])
+  vecMediaTemp[i] <- mediaTemp
+  cat("======>> mediaTemp do vecMediaTemp = ", vecMediaTemp[i])
+  vecMediaPluv[i] <- mediaPluv
+  cat("======>> mediaPluv do vecMediaPluv = ", vecMediaPluv[i])
 }
 
 # Inicio da imagem png
 png(sprintf("images/dispersaoTempVSChuvas.png"))
-  plot(vetor, vetor2,
+  plot(vecMediaTemp, vecMediaPluv,
   main=sprintf(":: Dispersão de Temperatura e Índice Pluviométrico ::"),
   lwd=3,
   xlab="Temperatura",
   ylab="Índice Pluviométrico")
   #grid cria quadrantes no gráfico, essas linhas da divisoria, terão a cor vermelha
   grid(col="red")
-
-  abline(lm(vetor2~vetor)) # reta de regressão linear no gráfico de dispersão
+  textxy(vecMediaTemp, vecMediaPluv,levsMes)
+  abline(lm(vecMediaPluv~vecMediaTemp)) # reta de regressão linear no gráfico de dispersão
 
   texto <- sprintf("Legenda")
   mtext(texto, side=1, valign="center", cex=0.8, halign= "left", mar=c(0,0,0,0), col="black", line=1)  
@@ -63,38 +52,21 @@ dev.off()
 # Fim da imagem png
 
 ##########################################################
-levsMes <- unique( unlist( lapply( database$mes , levels ) ) )
-cat("\n----------------------------------------------\n")
-cat("\nMenor, Maior e Média por Mês\n")
-for (i in levsMes) {
-  cat("\nMês: ", i,"\n")
-  mes <- subset(database, mes == i)
-
-  cat("\n+Temperatura: \n")
-  print("---Menor") 
-  print(min(as.numeric(sub(",",".",mes$temperatura))))
-  print("---Media") 
-  print(mean(as.numeric(sub(",",".",mes$temperatura))))
-  print("---Maior")
-  print(max(as.numeric(sub(",",".",mes$temperatura))))
-}
-
-# tam <- length(vetor2)
-# cat("Tamanho do vetor = ", tam)
-
-#png(file = "cidade.png")
-#plot(cid$mes, cid$temperatura, type="o", col="blue", 
-#  main="Cidade: \n",
-#  lwd=3,
-#  xlab="Mês",
-#  ylab="Temperatura")
-
-# grid cria quadrantes no gráfico, essas linhas da divisoria, terão a cor vermelha
-#grid(col="red")
-#dev.off()
-
 png(sprintf("images/testeBoxPlot.png"))
   boxplot(database, las = 2)
-
 dev.off()
 # Fim da imagem png
+
+cat("\n----------------------------------------------\n")
+print("Dados Calculados a partir de todas as entradas")
+print("=Temperatura")
+print("--Menor")
+print(min(database$pluviometrico))
+print("--Maior")
+print(max(database$pluviometrico))
+print("--Media")
+print(mean(database$pluviometrico))
+print("--Mediana")
+print(median(database$pluviometrico))
+print("--Moda")
+print(getmode(database$pluviometrico))
--- temperatura.R
+++ temperatura.R
@@ -1,3 +1,5 @@
+# Gerando gráfico de Mês por Temperatura
+# Calcula Maximas, Minimas, Medias, Medianas e Modas
 getmode <- function(v) {
   uniqv <- unique(v)
   uniqv[which.max(tabulate(match(v, uniqv)))]
@@ -5,43 +7,6 @@ getmode <- function(v) {
 
 database <- read.csv(file="embrapaFine.csv", header=TRUE, sep=";", col.names= c("mes","temperatura","pluviometrico","evaPotencial","armazenHidrico","evaReal","defHidrica","excedHidrico","cidade"), encoding= "UFT8")
 database$temperatura <- as.numeric(sub(",",".",database$temperatura))
-
-levsCidade <- unique( unlist( lapply( database$cidade , levels ) ) )
-
-cat("\n----------------------------------------------\n")
-for (i in levsCidade) {
-  cat("\nCidade: ", i,"\n")
-  cid <- subset(database, cidade == i)
-
-  png(sprintf("images/cidade/temperatura/%s.png",i))
-  plot(cid$temperatura, 
-  type="o",
-  col="blue", 
-  main=sprintf("%s\n Mês vs Temperatura\n", i),
-  lwd=3,
-  xlab="Mês",
-  ylab="Temperatura")
-  #grid cria quadrantes no gráfico, essas linhas da divisoria, terão a cor vermelha
-  grid(col="red")
-
-  texto <- sprintf("Média: %f", mean(cid$temperatura))
-  mtext(texto, side=1, valign="center", cex=0.8, halign= "left", mar=c(0,0,0,0), col="black", line=1)  
-
-  dev.off()
-
-  cat("\nMenor, Maior, Média, Mediana e Moda\n")
-  cat("\n+Temperatura: \n")
-  print("---Menor")
-  print(min(cid$temperatura))
-  print("---Maior")
-  print(max(cid$temperatura))
-  print("---Media")
-  print(mean(cid$temperatura))
-  print("--Mediana")
-  print(median(cid$temperatura))
-  print("--Moda")
-  print(getmode(cid$temperatura))
-}
 
 ##########################################################
 levsMes <- unique(database$mes)
@@ -69,7 +34,6 @@ for (i in levsMes) {
   dadosTemperatura <- rbind(dadosTemperatura, tmp)
 }
 cat("\n----------------------------------------------\n")
-print(dadosTemperatura)
 
 png(sprintf("images/temperaturaPorMes.png"))
 plot(c(1,12), c(15,30), 
