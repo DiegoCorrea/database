4d6b9c710de216bfab4d516743520d9940c48c0d
Diego Corrêa <diegocorrea.cc@gmail.com>
2017-03-21 20:25:26 -0300

    iniciando o terceiro entregavel

---
+++ 3- Entregavel/scripts/postgres/CREATETABLE.sql
@@ -0,0 +1,44 @@
+CREATE TABLE embrapa (
+mes VARCHAR(3),
+temperatura FLOAT,
+pluviometrico INTEGER,
+evaPotencial INTEGER,
+armazenHidrico INTEGER,
+evaReal INTEGER,
+defHidrica INTEGER,
+excedHidrico INTEGER,
+cidade VARCHAR(32)
+);
+CREATE TABLE embrapa2 (
+mes VARCHAR(3),
+temperatura FLOAT,
+pluviometrico INTEGER,
+evaPotencial INTEGER,
+armazenHidrico INTEGER,
+evaReal INTEGER,
+defHidrica INTEGER,
+excedHidrico INTEGER,
+cidade VARCHAR(32)
+);
+CREATE TABLE embrapa3 (
+mes VARCHAR(3),
+temperatura FLOAT,
+pluviometrico INTEGER,
+evaPotencial INTEGER,
+armazenHidrico INTEGER,
+evaReal INTEGER,
+defHidrica INTEGER,
+excedHidrico INTEGER,
+cidade VARCHAR(32)
+);
+CREATE TABLE embrapinha (
+mes VARCHAR(3),
+temperatura FLOAT,
+pluviometrico INTEGER,
+evaPotencial INTEGER,
+armazenHidrico INTEGER,
+evaReal INTEGER,
+defHidrica INTEGER,
+excedHidrico INTEGER,
+cidade VARCHAR(32)
+);
---
+++ 3- Entregavel/scripts/postgres/IMPORTCSV.sql
@@ -0,0 +1,4 @@
+\COPY embrapa FROM 'embrapaClean100x.csv' DELIMITER ',' CSV HEADER;
+\COPY embrapa2 FROM 'embrapaClean100x.csv' DELIMITER ',' CSV HEADER;
+\COPY embrapa3 FROM 'embrapaClean100x.csv' DELIMITER ',' CSV HEADER;
+\COPY embrapinha FROM 'embrapaClean.csv' DELIMITER ',' CSV HEADER;
