f6f3bfb928b467c61c258d494a097a7d8adfe0ad
Diego Correa <diegocorrea.cc@gmail.com>
2017-03-21 21:14:00 -0300

    adicionando roteiro como markdown

--- 3- Entregavel/scripts/postgres/CREATETABLE.sql
+++ 3- Entregavel/scripts/postgres/CREATETABLE.sql
@@ -1,37 +1,37 @@
-CREATE TABLE embrapa (
-mes VARCHAR(3),
-temperatura FLOAT,
-pluviometrico INTEGER,
-evaPotencial INTEGER,
-armazenHidrico INTEGER,
-evaReal INTEGER,
-defHidrica INTEGER,
-excedHidrico INTEGER,
-cidade VARCHAR(32)
-);
-CREATE TABLE embrapa2 (
-mes VARCHAR(3),
-temperatura FLOAT,
-pluviometrico INTEGER,
-evaPotencial INTEGER,
-armazenHidrico INTEGER,
-evaReal INTEGER,
-defHidrica INTEGER,
-excedHidrico INTEGER,
-cidade VARCHAR(32)
-);
-CREATE TABLE embrapa3 (
-mes VARCHAR(3),
-temperatura FLOAT,
-pluviometrico INTEGER,
-evaPotencial INTEGER,
-armazenHidrico INTEGER,
-evaReal INTEGER,
-defHidrica INTEGER,
-excedHidrico INTEGER,
-cidade VARCHAR(32)
-);
 CREATE TABLE embrapinha (
+mes VARCHAR(3),
+temperatura FLOAT,
+pluviometrico INTEGER,
+evaPotencial INTEGER,
+armazenHidrico INTEGER,
+evaReal INTEGER,
+defHidrica INTEGER,
+excedHidrico INTEGER,
+cidade VARCHAR(32)
+);
+CREATE TABLE embrapadez (
+mes VARCHAR(3),
+temperatura FLOAT,
+pluviometrico INTEGER,
+evaPotencial INTEGER,
+armazenHidrico INTEGER,
+evaReal INTEGER,
+defHidrica INTEGER,
+excedHidrico INTEGER,
+cidade VARCHAR(32)
+);
+CREATE TABLE embrapacem (
+mes VARCHAR(3),
+temperatura FLOAT,
+pluviometrico INTEGER,
+evaPotencial INTEGER,
+armazenHidrico INTEGER,
+evaReal INTEGER,
+defHidrica INTEGER,
+excedHidrico INTEGER,
+cidade VARCHAR(32)
+);
+CREATE TABLE embrapamil (
 mes VARCHAR(3),
 temperatura FLOAT,
 pluviometrico INTEGER,
